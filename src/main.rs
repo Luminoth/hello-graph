2367c2971db41247203430e9c0d2903241df7eab
Shane Lillie <ignignokterr@gmail.com>
2022-11-14 21:02:48 -0800

    Start with a basic warp example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(warnings)]
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let routes = warp::any().map(|| "Hello, World!");
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
